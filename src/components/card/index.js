b73f966fc293b630757743e63460f418c9651a87
Keylane <keylanessilva07@gmail.com>
2021-09-14 01:53:10 -0300

    trabalhando com components

---
+++ src/components/card/index.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import Button from '../button/index';
+
+const Card = () => {
+
+    const [valor, setValor] = useState(0);
+
+    function Adicionar(){
+        setValor(valor + 1);
+    }
+
+    function Remover(){
+        setValor(valor - 1);
+    }
+
+    return(
+        <div className="card">
+        <div className="card-header">
+            Card
+        </div>
+        <div className="card-body">
+            <p>Valor: {valor}</p>
+            <Button 
+                className="btn btn-success"
+                onClick={Adicionar}> 
+                    Adicionar
+            </Button>
+            <Button
+                className="btn btn-danger"
+                onClick={Remover}> 
+                    Remover
+            </Button>
+            
+        </div>
+        </div>
+        )
+}
+
+export default Card;
---
+++ src/components/item/index.js
@@ -0,0 +1,9 @@
+const Item = ({ p }) => {
+    return(
+        <a href="#" className="list-group-item list-group-item-action list-group-item-dark">
+        {p}
+      </a>
+    )
+} 
+
+export default Item;
